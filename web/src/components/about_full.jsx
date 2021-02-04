a4c8aa5df1473c548d30055359a160cf24309cfc
camthegeek <cam@camthegeek.net>
2021-02-04 14:04:40 -0500

    [about_full] rels on a

--- web/src/components/about_full.jsx
+++ web/src/components/about_full.jsx
@@ -11,10 +11,10 @@ class AboutBox extends Component {
                             <Card.Body>
                                 <Card.Text>
                                     Cameron "camthegeek" Chadwell is a web developer from a small town in Tennessee. Currently, he
-                                is employed as a Web Developer / Program Manager for <a href="https://mrscorp.com" target="_blank">Mission Ready Solutions, Inc.</a>
+                                is employed as a Web Developer / Program Manager for <a href="https://mrscorp.com" rel="noreferrer" target="_blank">Mission Ready Solutions, Inc.</a>
                                 His biggest project to date is developing a personal protective equipment store for Lyft which was aptly
-                                named <a href="https://lyft-store.com" target="_blank">Lyft Store</a>. He also enjoys developing for cryptocurrency projects such as the privacy-oriented
-                                project, <a href="https://getmasari.org" target="_blank">Masari</a>, as well as the file storage project, <a href="https://scpri.me" target="_blank">ScPrime</a>.
+                                named <a href="https://lyft-store.com" rel="noreferrer" target="_blank">Lyft Store</a>. He also enjoys developing for cryptocurrency projects such as the privacy-oriented
+                                project, <a href="https://getmasari.org" rel="noreferrer" target="_blank">Masari</a>, as well as the file storage project, <a rel="noreferrer" href="https://scpri.me" target="_blank">ScPrime</a>.
                             </Card.Text>
                             </Card.Body>
                         </Card>
