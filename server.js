f9e8cfc641498d212ae39e75e1e0ad31a8e64818
camthegeek <cam@camthegeek.net>
2021-02-04 21:32:23 -0500

    another attempt at meta being correct

--- server.js
+++ server.js
@@ -10,4 +10,36 @@ app.get('*', function(request, response) {
   response.sendFile(filePath);
 });
 
+app.get('/', function(request, response) {
+    console.log('Home page visited!');
+    const filePath = path.resolve(__dirname, './web/build', 'index.html');
+  
+    // read in the index.html file
+    fs.readFile(filePath, 'utf8', function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+      
+      // replace the special strings with server generated strings
+      data = data.replace(/\$OG_TITLE/g, 'Home Page');
+      data = data.replace(/\$OG_DESCRIPTION/g, "Home page description");
+      result = data.replace(/\$OG_IMAGE/g, 'https://i.imgur.com/V7irMl8.png');
+      response.send(result);
+    });
+  });
+
+app.get('/about', function(request, response) {
+    console.log('About page visited!');
+    const filePath = path.resolve(__dirname, './web/build', 'index.html')
+    fs.readFile(filePath, 'utf8', function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+      data = data.replace(/\$OG_TITLE/g, 'About Page');
+      data = data.replace(/\$OG_DESCRIPTION/g, "About page description");
+      result = data.replace(/\$OG_IMAGE/g, 'https://i.imgur.com/V7irMl8.png');
+      response.send(result);
+    });
+  });
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
