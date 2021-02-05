db8af5a61673bdea1d724058b01dd4cae090a99c
camthegeek <cam@camthegeek.net>
2021-02-04 21:43:10 -0500

    prerender stuff again

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+const config = require('./config.json');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const path = require('path');
 
 app.use(express.static(path.resolve(__dirname, './web/build')));
-app.use(require('prerender-node'));
+app.use(require('prerender-node').set('prerenderToken', config.prerender.token));
 
 app.get('*', function(request, response) {
   const filePath = path.resolve(__dirname, './web/build', 'index.html');
